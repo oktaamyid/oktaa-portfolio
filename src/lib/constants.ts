056324ad99fa24117df107a1a6351535b175ea1b
Tayen15 <tiantiantitik@gmail.com>
2025-05-08 22:28:50 +0700

    Fix URLs in SOCIAL_MEDIA constants for consistency

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,8 +2,8 @@ export const MY_NAME = "Firtiansyah Okta Resama";
 export const MY_LOCATION = "Depok, Indonesia";
 export const SOCIAL_MEDIA = {
      github: "https://github.com/Tayen15",
-     linkedin: "https://linkedin.com/in/firtiansyahokta",
+     linkedin: "https://www.linkedin.com/in/firtiansyahokta/",
      twitter: "https://twitter.com/username",
-     instagram: "https://instagram.com/kyonezet_",
+     instagram: "https://www.instagram.com/kyonezet_",
      email: "mailto:tianresama@gmail.com"
 };
